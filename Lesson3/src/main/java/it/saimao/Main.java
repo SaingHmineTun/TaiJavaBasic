ade4ca7869995090e7a4d3edc0324332fc1493a7
Mein <saisainghminetun@gmail.com>
2023-09-11 15:38:29 +0630

    Initial commit

---
+++ Lesson3/src/main/java/it/saimao/Main.java
@@ -0,0 +1,11 @@
+package it.saimao;
+
+public class Main {
+    public static void main(String[] args) {
+        var hello = "Hello World";
+        var version = 20;
+        version = 25;
+        System.out.println(hello);
+        System.out.println(version);
+    }
+}
