a17e8cde35b9f559085b6a6dfb671aae1c46a56a
Mein <saisainghminetun@gmail.com>
2023-09-29 15:29:54 +0630

    Adding new lessons from Lesson 20 - 23

---
+++ Lesson23/src/main/java/it/saimao/Main.java
@@ -0,0 +1,11 @@
+package it.saimao;
+public class Main {
+    public static void main(String[] args) {
+        // 1. ႁဵတ် do - while loop
+        char c = 'a';
+        do {
+            System.out.println(c);
+            c ++;
+        } while (c <= 'z');
+    }
+}
